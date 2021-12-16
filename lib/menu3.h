0dcd84c0fdca9569b7820ac6fe4544b0b3ea17e5
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2021-12-16 13:39:28 +0800

    just add little little things

--- lib/menu3.h
+++ lib/menu3.h
@@ -2,9 +2,13 @@
     #define __MENU3_
     #include "data_type.h"
     #include "menu3.c"
+
+    // dekla here
+
     void editdata(data_t **main_node);
     void editbyDeadline (data_t **main_node);
     void editbyPriority (data_t **main_node);
     
-    
+    // dekla end
+
 #endif
