7da1e676a49f99a32ba0ceab5c69bbbb9b0cce32
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2021-12-16 09:30:31 +0800

    Insert main things
    Insert main things

---
+++ lib/menu3.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <final_module.h>
+#include <time.h>
+#include "data_type.h"
+
+//code below here //
+
+
+//code selesai
---
+++ lib/menu3.h
@@ -0,0 +1,10 @@
+#ifndef __MENU2_
+    #define __MENU3_
+    #include "data_type.h"
+    #include "menu3.c"
+    void editdata(data_t **main_node);
+    void editbyDeadline (data_t **main_node);
+    void editbyPriority (data_t **main_node);
+    
+    
+#endif
