b5cbf6a3b458c44eac4ddb3abefa0b05029cec0e
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2021-12-16 09:54:19 +0800

    Just update a litte shit

--- lib/menu3.c
+++ lib/menu3.c
@@ -10,7 +10,27 @@ void editdata(data_t **main_node){
     data_t *temp = *main_node;
     char nama_tugas[255], find[255];
     printf("Input Nama Tugas yang ingin Dicari : ");
-    scanf("%[^\n]", &nama_tugas);
+    scanf("%[^\n]", &nama_tugas); fflush(stdin);
+
+    while (temp!=NULL)
+    {
+        // code here
+        if (strcmp(temp->nama_tugas,find)==0){
+            printf("Masukan Tugas baru : ");
+            scanf("%[^\n]", &find); fflush(stdin);
+            getchar();
+            strcpy(temp->nama_tugas, find);
+            printf("Tugas baru berhasil di simpan");
+        }
+        temp=temp->next;
+        
+    }
+    if(temp == NULL){
+        printf("Data tidak ditemukan.\n");
+    }
+
 }
+
+
 
 //code selesai
