409473bf2b1fd760b062f480e96214b7579643ca
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2021-12-16 13:03:47 +0800

    Just diupdate again, nothing more

--- lib/menu3.c
+++ lib/menu3.c
@@ -9,18 +9,18 @@
 void editdata(data_t **main_node){
     data_t *temp = *main_node;
     char nama_tugas[255], find[255];
-    printf("Input Nama Tugas yang ingin Dicari : ");
+    printf("Input Nama Tugas yang ingin Dicari : \n");
     scanf("%[^\n]", &nama_tugas); fflush(stdin);
 
     while (temp!=NULL)
     {
         // code here
         if (strcmp(temp->nama_tugas,find)==0){
-            printf("Masukan Tugas baru : ");
+            printf("Masukan Tugas baru : \n");
             scanf("%[^\n]", &find); fflush(stdin);
             getchar();
             strcpy(temp->nama_tugas, find);
-            printf("Tugas baru berhasil di simpan");
+            printf("Tugas baru berhasil di simpan \n");
         }
         temp=temp->next;
         
@@ -29,6 +29,32 @@ void editdata(data_t **main_node){
         printf("Data tidak ditemukan.\n");
     }
 
+}
+
+void editbyPrioritas(data_t **main_node){
+
+    //code here
+    data_t *temp = *main_node;
+    int prioritas;
+    char find[255];
+    printf("Masukan Nama Tugas yang Ingin dicari : \n");
+    scanf("%[^\n]", &find); fflush(stdin);
+
+    while (temp!=NULL)
+    {
+        if(strcmp(temp->nama_tugas,find) == 0){
+            printf("Masukan Nilai Prioritas Baru (1-4) : \n");
+            scanf("%d", &prioritas); fflush(stdin);
+            getchar();
+            temp->priority = prioritas;
+            printf("Data Baru tersimpan.\n");
+        }
+        temp=temp->next;
+    }
+    if (temp==NULL){
+        printf("Data Yang dicari tidak ada.\n");
+    }
+    
 }
 
 
