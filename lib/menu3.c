693756aebd815bf279a60f1cccca2f39d6530bbb
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2021-12-16 14:09:32 +0800

    just update little shittt

--- lib/menu3.c
+++ lib/menu3.c
@@ -31,6 +31,52 @@ void editdata(data_t **main_node){
 
 }
 
+
+
+void editbyDeadline (data_t **main_node){
+    data_t *temp = *main_node, input;
+    date tanggal;
+    getTheDate(&tanggal);
+    char find[255];
+    printf("Masukan Nama Tugas yang ingin dicari : \n");
+    scanf("%[^\n]", &find); fflush(stdin);
+    getchar();
+
+    while(temp!=NULL){
+        if(strcmp(temp->nama_tugas,find) == 0){
+
+            while (1)
+            {
+                printf("Masukan Tanggal Deadline baru (DD/MM/YY) : \n");
+                scanf("%d%d%d", &input.dl_dd, &input.dl_mm, &input.dl_yyyy); fflush(stdin);
+                getchar();
+
+                if (input.dl_dd < tanggal.dd && input.dl_mm < tanggal.mm && input.dl_yyyy < tanggal.yyyy) 
+                {
+                    printf("Tanggal tidak boleh sama sebelum hari ini.\n");
+                    continue;
+                }else {
+                    temp->dl_dd = input.dl_dd;
+                    temp->dl_mm = input.dl_mm;
+                    temp->dl_yyyy = input.dl_yyyy;
+                    printf("Data Berhasil Disimpan. \n");
+                    break;
+                }
+                
+            }
+            return;
+
+        }
+        temp=temp->next;
+    }
+
+
+
+}
+
+
+
+
 void editbyPrioritas(data_t **main_node){
 
     //code here
